4a41903810ae7ade854f07b379e3094ca4caf2e5
Somoreshm <Somoresh.m6@gmail.com>
2026-02-13 18:53:00 +0530

    Make VARIABLES template non-public for runtime injection

--- src/main/java/config/VARIABLES_TEMPLATE.java
+++ src/main/java/config/VARIABLES_TEMPLATE.java
@@ -1,9 +1,23 @@
 package config;
 
-public class VARIABLES {
+/*
+ This file exists only so Maven can compile the project in Git.
+ Real VARIABLES.java will be injected locally on each machine.
+*/
 
-    public static String USERNAME = "LOCAL_USERNAME";
-    public static String PASSWORD = "LOCAL_PASSWORD";
-    public static String DOWNLOAD_PATH = "C:\\TEMP";
+class VARIABLES {
 
+    static String EXCEL_FILE_PATH = "";
+    static String VOTER_FILE_PATH = "";
+    static String AADHAR_FILE_PATH = "";
+    static String PARCHA_FILE_PATH = "";
+    static String BANK_FILE_PATH = "";
+
+    static String SIGN_IN_PAGE_URL = "";
+    static String NEW_REGISTRATION_URL = "";
+
+    static String EMAIL = "";
+    static String PASSWORD = "";
+
+    static String SHEET_NAME = "";
 }
